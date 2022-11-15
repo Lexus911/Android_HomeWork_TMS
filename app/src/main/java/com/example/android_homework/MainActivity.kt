2de7b90febef23d821ada3d7cd0aa6e8300296f7
Alexey Matskevich <61157700+Lexus911@users.noreply.github.com>
2022-11-15 22:48:54 +0300

    HomeWork android lesson 2

--- app/src/main/java/com/example/android_homework/MainActivity.kt
+++ app/src/main/java/com/example/android_homework/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.android_homework
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
+import com.example.android_homework.MainActivity2.Companion.startMainActivityTextView
+import com.example.android_homework.MainActivity2.Companion.startMainActivityToolBar
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,7 +21,29 @@ class MainActivity : AppCompatActivity() {
 
         btnGoToActivity2.setOnClickListener{
             startActivity(
-                Intent("OPEN_ACTIVITY2"))
+                Intent(getString(R.string.open_act2_marker)))
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.item_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val textView1 = findViewById<TextView>(R.id.textView1)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+
+        when(item.itemId){
+            R.id.actionBar -> startMainActivityToolBar(this, "Second item clicked")
+            R.id.texView -> startMainActivityTextView(this,textView1.text.toString(),textView2.text.toString())
+            R.id.close -> finishAffinity()
+            R.id.goToActivity3 -> startActivity(Intent(this, MainActivity3::class.java)
+            )
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
+
--- app/src/main/java/com/example/android_homework/MainActivity2.kt
+++ app/src/main/java/com/example/android_homework/MainActivity2.kt
@@ -1,9 +1,11 @@
 package com.example.android_homework
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,37 @@ class MainActivity2 : AppCompatActivity() {
             startActivity(
                 Intent(this, MainActivity3::class.java)
             )
+        }
+
+        val textView3 = findViewById<TextView>(R.id.textView3)
+        val textView4 = findViewById<TextView>(R.id.textView4)
+
+        val data: String = intent.getStringExtra(KEY) ?: getString(R.string.no_data)
+        textView3.text = data
+        val data2: String = intent.getStringExtra(KEY2) ?: getString(R.string.no_data)
+        textView4.text = data2
+        val data3: String = intent.getStringExtra(KEY3) ?: getString(R.string.android_homework_toolbar)
+        supportActionBar?.title = data3
+    }
+
+    companion object{
+
+        private const val KEY = "1"
+        private const val KEY2 = "2"
+        private const val KEY3 = "3"
+
+        fun startMainActivityTextView(context: Context, string: String, string2: String){
+
+            val intent = Intent(context, MainActivity2::class.java)
+            intent.putExtra(KEY, string)
+            intent.putExtra(KEY2,string2)
+            context.startActivity(intent)
+        }
+        fun startMainActivityToolBar(context: Context, string: String){
+
+            val intent = Intent(context, MainActivity2::class.java)
+            intent.putExtra(KEY3, string)
+            context.startActivity(intent)
         }
     }
 }
